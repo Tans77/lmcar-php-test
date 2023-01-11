d4f1184d341432aac7a5a585a78da37331062cbe
wuyueqi <wuyueqi@changba.com>
2023-01-11 13:05:14 +0800

    题目1

--- src/Service/ProductHandler.php
+++ src/Service/ProductHandler.php
@@ -4,5 +4,41 @@ namespace App\Service;
 
 class ProductHandler
 {
-
+    public function getTotalPrice(array $products) : int
+    {
+        $totalPrice = 0;
+        foreach ($products as $product) {
+            $price = $product['price'] ?: 0;
+            $totalPrice += $price;
+        }
+        return $totalPrice;
+    }
+    
+    public function sortAndDessert($products) : array
+    {
+        usort($products, function($a, $b) {
+            $aPrice = $a['price'];
+            $bPrice = $b['price'];
+            if ($aPrice == $bPrice)
+                return 0;
+            return ($aPrice > $bPrice) ? -1 : 1;
+        });
+        $resList =[];
+        foreach ($products as  $product) {
+            if(isset($product['type']) && $product['type'] == 'Dessert'){
+                $resList[] = $product ;
+            }
+        }
+        return $resList;
+    }
+    
+    public function listStrToTime(array $products) : array
+    {
+        foreach ($products as $index => $product) {
+            if(isset($product['create_at'])){
+                $products[$index]['create_at'] = strtotime($product['create_at']); ;
+            }
+        }
+        return $products;
+    }
 }
--- tests/Service/ProductHandlerTest.php
+++ tests/Service/ProductHandlerTest.php
@@ -10,6 +10,7 @@ use App\Service\ProductHandler;
  */
 class ProductHandlerTest extends TestCase
 {
+    //原始数据
     private $products = [
         [
             'id' => 1,
@@ -54,15 +55,96 @@ class ProductHandlerTest extends TestCase
             'create_at' => '2021-04-04 19:23:00',
         ],
     ];
+    //需求2结果，用于对比
+    private $sortListRes =[
+        [
+            'id' => 5,
+            'name' => 'New York Cheese Cake',
+            'type' => 'Dessert',
+            'price' => 40,
+            'create_at' => '2021-04-19 14:38:00',
+        ],
+        [
+            'id' => 4,
+            'name' => 'Cup cake',
+            'type' => 'Dessert',
+            'price' => 35,
+            'create_at' => '2021-04-18 08:45:00',
+        ],
+    
+    ];
+    //需求3结果，用于对比
+    private $strToTimeRes =[
+        [
+            'id' => 1,
+            'name' => 'Coca-cola',
+            'type' => 'Drinks',
+            'price' => 10,
+            'create_at' => 1618912800,
+        ],
+        [
+            'id' => 2,
+            'name' => 'Persi',
+            'type' => 'Drinks',
+            'price' => 5,
+            'create_at' => 1618995600,
+        ],
+        [
+            'id' => 3,
+            'name' => 'Ham Sandwich',
+            'type' => 'Sandwich',
+            'price' => 45,
+            'create_at' => 1618945200,
+        ],
+        [
+            'id' => 4,
+            'name' => 'Cup cake',
+            'type' => 'Dessert',
+            'price' => 35,
+            'create_at' => 1618735500,
+        ],
+        [
+            'id' => 5,
+            'name' => 'New York Cheese Cake',
+            'type' => 'Dessert',
+            'price' => 40,
+            'create_at' => 1618843080,
+        ],
+        [
+            'id' => 6,
+            'name' => 'Lemon Tea',
+            'type' => 'Drinks',
+            'price' => 8,
+            'create_at' => 1617564180,
+        ],
+    ];
+    
 
     public function testGetTotalPrice()
     {
-        $totalPrice = 0;
-        foreach ($this->products as $product) {
-            $price = $product['price'] ?: 0;
-            $totalPrice += $price;
-        }
-
+        $productHandler = new ProductHandler();
+        $totalPrice = $productHandler->getTotalPrice($this->products);
         $this->assertEquals(143, $totalPrice);
     }
+    
+    public function testSortAndDessert()
+    {
+        $productHandler = new ProductHandler();
+        $resList = $productHandler->sortAndDessert($this->products);
+        $this->assertEquals($this->sortListRes, $resList);
+    }
+    
+    public function testListStrToTime()
+    {
+        $productHandler = new ProductHandler();
+        $resList = $productHandler->listStrToTime($this->products);
+        $this->assertEquals($this->strToTimeRes, $resList);
+    }
+    
+    
+    
+    
+    
+    
+    
 }
